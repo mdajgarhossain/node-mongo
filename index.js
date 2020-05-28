d65128a4857f0accf768864f0a7930520059a4b5
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-05-28 16:24:40 +0600

    Create POST api, install postman, read post data, use body parser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const users = ['Karim', 'Rahim', 'Jabbar', 'Jewell'];
 
@@ -16,20 +19,31 @@ app.get('/', (req, res) => {
     res.send(developer);
 });
 
-app.get('/devs/details', (req, res) => {
-    const frontend = {
-        language: 'Javascript',
-        framework: 'React'
-    };
-    res.send(frontend);
-});
+// app.get('/devs/details', (req, res) => {
+//     const frontend = {
+//         language: 'Javascript',
+//         framework: 'React'
+//     };
+//     res.send(frontend);
+// });
 
 //dynamic api, api parameter, access params, access query
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
-    console.log(req.query);
+    // console.log(req.query);
     const name = users[id];
     res.send({id, name});
+});
+
+//post
+app.post('/addUser', (req, res) => {
+    //receive data from client
+    console.log('data received', req.body);
+    //save to database
+    const user = req.body;
+    user.id = 7;
+    //send back to the client
+    res.send(user);
 });
 
 app.listen(3000, () => console.log('Server is starting...'));
