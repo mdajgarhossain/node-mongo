83b9fc28769adf4afb18842ec08eda98963d47e6
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-06-08 20:25:52 +0600

    Load database data, load partial data using limit,filter data, hide sensitive data

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+//hide sensitive data
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+//private data
+//hide sensitive data with dotenv
+const uri = process.env.DB_PATH;
+
+let client = new MongoClient(uri, { useNewUrlParser: true });
 const users = ['Karim', 'Rahim', 'Jabbar', 'Jewell'];
 
 //api return object, create multiple api
@@ -19,13 +27,30 @@ app.get('/', (req, res) => {
     res.send(developer);
 });
 
-// app.get('/devs/details', (req, res) => {
-//     const frontend = {
-//         language: 'Javascript',
-//         framework: 'React'
-//     };
-//     res.send(frontend);
-// });
+app.get('/devs/details', (req, res) => {
+    const frontend = {
+        language: 'Javascript',
+        framework: 'React'
+    };
+    res.send(frontend);
+});
+
+app.get('/products', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.find({name:'Mobile'}).limit(10).toArray((err, documents) => {
+            if(err) {
+                console.log(err);
+                res.status(500).send({message: err});
+            } else {
+                console.log('Products extracted from database', documents);
+                res.send(documents);
+            }
+        })
+        client.close();
+    });
+});
 
 //dynamic api, api parameter, access params, access query
 app.get('/users/:id', (req, res) => {
@@ -36,14 +61,27 @@ app.get('/users/:id', (req, res) => {
 });
 
 //post
-app.post('/addUser', (req, res) => {
-    //receive data from client
-    console.log('data received', req.body);
+app.post('/addProduct', (req, res) => {
     //save to database
-    const user = req.body;
-    user.id = 7;
-    //send back to the client
-    res.send(user);
+    const product = req.body;
+    console.log('data received', product);
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.insertOne(product, (err, result) => {
+            if(err) {
+                console.log(err);
+                res.status(500).send({message: err});
+            } else {
+                console.log('Successfully inserted', result);
+                res.send(result.ops[0]);
+            }
+        })
+        client.close();
+    });
 });
 
-app.listen(3000, () => console.log('Server is starting...'));
+
+const port = process.env.PORT || 4200;
+app.listen(port, () => console.log('Server is starting...'));
